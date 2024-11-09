0e7fae4953348e3c7137a5599af463e6781251f6
Ingrid Skeide <79411105+ingrid-skeide@users.noreply.github.com>
2024-11-09 13:26:11 +0100

    Migrated old eslint to new config and configured jest with baseline test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,68 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import cypress from 'eslint-plugin-cypress';
+import jestPlugin from 'eslint-plugin-jest';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  ...compat.extends('eslint:recommended'),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  {
+    files: ['**/*.test.js'],
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        test: 'readonly',
+        expect: 'readonly'
+      },
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
+    }
+  },
+  ...compat.extends('plugin:cypress/recommended').map((config) => ({
+    ...config,
+    files: ['**/*.cy.js'],
+  })),
+  {
+    files: ['**/*.cy.js'],
+
+    plugins: {
+      cypress,
+    },
+
+    languageOptions: {
+      globals: {
+        ...cypress.environments.globals.globals,
+      },
+    },
+
+    rules: {
+      'cypress/no-unnecessary-waiting': 'off',
+      'no-unused-vars': 'off',
+    },
+  },
 ];
